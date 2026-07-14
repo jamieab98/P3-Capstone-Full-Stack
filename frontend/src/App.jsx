6daad5326071ae4e247712c4b10dc2e0e0130871
Jamie Bryson <jamieab98@gmail.com>
2026-07-14 19:36:17 -0400

    cleaned up some of te coding in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import AssignedTasks from "./components/AssignedTasks"
 import Login from "./components/Login"
 import UserDashboard from "./components/UserDashboard"
 import UserTasks from "./components/UserTasks"
+import AssignedTasks from "./components/AssignedTasks"
 
 function App(){
 
   return(
     <>
       <h1>App</h1>
-      <AssignedTasks/>
+      <Login/>
     </>
   )
 }
