37c3e1715a20c156c9de3a20fcd0fb19c0351f95
Jamie Bryson <jamieab98@gmail.com>
2026-07-28 18:27:47 -0400

    made the basics of models.py

--- backend/models.py
+++ backend/models.py
@@ -2,4 +2,5 @@ from extensions import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(20), nullable=False)
