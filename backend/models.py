38cd672e47862194d11f66272f5b6cbe0430074c
Jamie Bryson <jamieab98@gmail.com>
2026-07-28 18:41:42 -0400

    started creating AssignedTask model and linking it to models.py

--- backend/models.py
+++ backend/models.py
@@ -4,3 +4,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(20), nullable=False)
+    aTasks = db.relationship("AssignedTask", backref="user", lazy=True)
+
+class AssignedTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    aTaskDescription = db.Column(db.String, nullable=False)
+    ownerId = db.Column(db.Integer, db.Foreignkey("user.id"), nullable=False)
