02531586e6f59aba3c28db91366f32bd4240b9ae
Jamie Bryson <jamieab98@gmail.com>
2026-07-30 18:33:52 -0400

    finished creating the models

--- backend/models.py
+++ backend/models.py
@@ -4,9 +4,18 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(20), nullable=False)
-    aTasks = db.relationship("AssignedTask", backref="user", lazy=True)
+    assigned_tasks = db.relationship("AssignedTask", backref="user", lazy=True)
+    daily_tasks = db.relationship("DailyTask", backref="user", lazy=True)
 
 class AssignedTask(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    aTaskDescription = db.Column(db.String, nullable=False)
-    ownerId = db.Column(db.Integer, db.Foreignkey("user.id"), nullable=False)
+    assigned_task_description = db.Column(db.String, nullable=False)
+    owner_id = db.Column(db.Integer, db.Foreignkey("user.id"), nullable=False)
+    completion_status = db.Column(db.Boolean, nullable=False)
+
+class DailyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    daily_task_description = db.Column(db.String, nullable=False)
+    owner_id = db.Column(db.Integer, db.Foreignkey("user.id"), nullable=False)
+    completion_status = db.Column(db.Boolean, nullable=False)
+    dates_completed = db.Column(db.JSON, default=list)
