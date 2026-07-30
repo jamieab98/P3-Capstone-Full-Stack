47a88ed69e46929f661790ff318477ef194c85d0
Jamie Bryson <jamieab98@gmail.com>
2026-07-30 18:38:24 -0400

    added the migrate line to app.py. Also created the database

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
+migrate.init_app(app, db)
 cors(app)
 api = Api(app)
 
