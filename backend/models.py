f8b43c51f4f57e4ad51fedb4a5cb2a96bb1e4b83
Jamie Bryson <jamieab98@gmail.com>
2026-07-31 09:27:29 -0400

    started seeding the database

--- backend/models.py
+++ backend/models.py
@@ -5,7 +5,7 @@ class User(db.Model):
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(20), nullable=False)
     manager = db.Column(db.Boolean, nullable=False)
-    workers = db.Column(db.JSON, default=list)
+    workers_id = db.Column(db.JSON, default=list, nullable=True)
     assigned_tasks = db.relationship("AssignedTask", backref="user", lazy=True)
     daily_tasks = db.relationship("DailyTask", backref="user", lazy=True)
 
@@ -13,11 +13,11 @@ class AssignedTask(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     assigned_task_description = db.Column(db.String, nullable=False)
     owner_id = db.Column(db.Integer, db.Foreignkey("user.id"), nullable=False)
-    completion_status = db.Column(db.Boolean, nullable=False)
+    completion_status = db.Column(db.Boolean, default=False)
 
 class DailyTask(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     daily_task_description = db.Column(db.String, nullable=False)
     owner_id = db.Column(db.Integer, db.Foreignkey("user.id"), nullable=False)
-    completion_status = db.Column(db.Boolean, nullable=False)
+    completion_status = db.Column(db.Boolean, default=False)
     dates_completed = db.Column(db.JSON, default=list)
--- backend/seed.py
+++ backend/seed.py
@@ -8,5 +8,19 @@ with app.app_context():
     AssignedTask.delete()
     DailyTask.delete()
 
-    u = User(username="jamieab98", password="123456")
-    
+    u1 = User(username="jamieab98", password="123456", manager=True, workers_id=[2, 3])
+    u2 = User(username="bobbyb44", password="6767", manager=False)
+    u3 = User(username="michaelb", password="durrr", manager=False)
+
+    a1 = AssignedTask(assiged_task_description="Find a pookie", owner_id=2)
+    a2 = AssignedTask(assigned_task_description="Date your pookie", owner_id=2)
+    a3 = AssignedTask(assigned_task_description="Stop bothering your pookie", owner_id=3)
+    a4 = AssignedTask(assigned_task_description="Finish putting your bed together", owner_id=3)
+
+    d1 = DailyTask(daily_task_description="Check and reply to emails as necessary", owner_id=1)
+    d2 = DailyTask(daily_task_description="Check and reply to emails as necessary", owner_id=2)
+    d3 = DailyTask(daily_task_description="Check and reply to emails as necessary", owner_id=3)
+    d4 = DailyTask(daily_task_description="Make sure Micahel is working", owner_id=2)
+    d5 = DailyTask(daily_task_description="Log your connections in production timer", owner_id=2)
+    d6 = DailyTask(daily_task_description="Sweep the kitchen", owner_id=3)
+    d7 = DailyTask(daily_task_description="Cout your chickens before they hatch", owner_id=3)
