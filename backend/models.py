060faf668159ed43e7dd0e6eb0281535aa0c22cf
Jamie Bryson <jamieab98@gmail.com>
2026-08-04 20:35:31 -0400

    added to_dict() to all the models

--- backend/models.py
+++ backend/models.py
@@ -10,12 +10,29 @@ class User(db.Model):
     assigned_tasks = db.relationship("AssignedTask", backref="user", lazy=True)
     daily_tasks = db.relationship("DailyTask", backref="user", lazy=True)
 
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "username": self.username,
+            "password": self.password,
+            "manager": self.manager,
+            "workers_id": self.workers_id
+        }
+
 class AssignedTask(db.Model):
     __tablename__ = "assigned_tasks"
     id = db.Column(db.Integer, primary_key=True)
     assigned_task_description = db.Column(db.String, nullable=False)
     owner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     completion_status = db.Column(db.Boolean, default=False)
+
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "assigned_task_description": self.assigned_task_description,
+            "owner_id": self.owner_id,
+            "completion_status": self.completion_status
+        }
 
 class DailyTask(db.Model):
     __tablename__ = "daily_tasks"
@@ -24,3 +41,12 @@ class DailyTask(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     completion_status = db.Column(db.Boolean, default=False)
     dates_completed = db.Column(db.JSON, default=list)
+
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "daily_task_description": self.daily_task_description,
+            "owner_id": self.owner_id,
+            "completion_status": self.completion_status,
+            "dates_completed": self.dates_completed
+        }
