a33ab08cde93adb2f143ddea64a89b0f0a037c25
Jamie Bryson <jamieab98@gmail.com>
2026-08-04 20:08:57 -0400

    quick save but I'm lost

--- backend/app.py
+++ backend/app.py
@@ -8,8 +8,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
-migrate.init_app(app, db)
 cors(app)
+migrate.init_app(app, db)
 api = Api(app)
 
 class Home(Resource):
--- backend/models.py
+++ backend/models.py
@@ -1,23 +1,26 @@
 from extensions import db
 
 class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(225), nullable=False)
     manager = db.Column(db.Boolean, nullable=False)
     workers_id = db.Column(db.JSON, default=list, nullable=True)
     assigned_tasks = db.relationship("AssignedTask", backref="user", lazy=True)
     daily_tasks = db.relationship("DailyTask", backref="user", lazy=True)
 
 class AssignedTask(db.Model):
+    __tablename__ = "assigned_tasks"
     id = db.Column(db.Integer, primary_key=True)
     assigned_task_description = db.Column(db.String, nullable=False)
-    owner_id = db.Column(db.Integer, db.Foreignkey("user.id"), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     completion_status = db.Column(db.Boolean, default=False)
 
 class DailyTask(db.Model):
+    __tablename__ = "daily_tasks"
     id = db.Column(db.Integer, primary_key=True)
     daily_task_description = db.Column(db.String, nullable=False)
-    owner_id = db.Column(db.Integer, db.Foreignkey("user.id"), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     completion_status = db.Column(db.Boolean, default=False)
     dates_completed = db.Column(db.JSON, default=list)
