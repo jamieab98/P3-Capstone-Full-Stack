c70e4bbe2b796597f848cf089ee0be0c47d671ce
Jamie Bryson <jamieab98@gmail.com>
2026-08-05 23:12:06 -0400

    got the number of incomplete tasks to display for the user on the dashboard

--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -4,12 +4,16 @@ import Logout from "./Logout"
 function UserDashboard({setView, userID}){
 
     const [userData, setUserData] = useState({})
+    const [incompleteTasks, setIncompleteTasks] = useState(0)
 
     useEffect(()=>{
         fetch(`http://127.0.0.1:5000/userdata/${userID}`)
         .then(response => response.json())
         .then(data => {
             setUserData(data)
+            const incompleteAssigned = data.assigned_tasks.filter(task => task.completion_status == false).length
+            const incompleteDaily = data.daily_tasks.filter(task => task.completion_status == false).length
+            setIncompleteTasks(incompleteAssigned + incompleteDaily)
         })
     }, [])
 
@@ -19,7 +23,7 @@ function UserDashboard({setView, userID}){
             <h2>User Dashboard Component</h2>
             <h3>Welcome {userData.username}!</h3>
             <h5>Employee ID: {userData.id}</h5>
-            <span>You have  tasks to complete</span>
+            <span>You have {incompleteTasks} tasks to complete</span>
         </>
     )
 }
