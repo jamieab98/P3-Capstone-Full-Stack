06f84072ecfff056d92f5732c3c73cb416f28603
Jamie Bryson <jamieab98@gmail.com>
2026-08-05 20:57:55 -0400

    Set up UserDashboard to show get the data when the user signs in

--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -1,14 +1,24 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Logout from "./Logout"
 
-function UserDashboard({setView}){
+function UserDashboard({setView, userID}){
+
+    const [userData, setUserData] = useState({})
+
+    useEffect(()=>{
+        fetch(`http://127.0.0.1:5000/userdata/${userID}`)
+        .then(response => response.json())
+        .then(data => {
+            setUserData(data)
+        })
+    }, [])
 
     return(
         <>
             <Logout setView={setView}/>
             <h2>User Dashboard Component</h2>
-            <h3>Welcome !</h3>
-            <h5>Employee ID: </h5>
+            <h3>Welcome {userData.username}!</h3>
+            <h5>Employee ID: {userData.id}</h5>
             <span>You have  tasks to complete</span>
         </>
     )
