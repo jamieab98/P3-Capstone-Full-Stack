ef01ef55f627f9ca3464cd748ac6541a74ada9b9
Jamie Bryson <jamieab98@gmail.com>
2026-08-15 14:43:05 -0400

    changed seed.py

--- backend/seed.py
+++ backend/seed.py
@@ -11,10 +11,10 @@ with app.app_context():
     u2 = User(username="bobbyb44", password="6767", manager=True)
     u3 = User(username="michaelb", password="durrr", manager=False)
 
-    a1 = AssignedTask(assigned_task_description="Find a pookie", owner_id=2)
-    a2 = AssignedTask(assigned_task_description="Date your pookie", owner_id=2)
-    a3 = AssignedTask(assigned_task_description="Stop bothering your pookie", owner_id=3)
-    a4 = AssignedTask(assigned_task_description="Finish putting your bed together", owner_id=3)
+    a1 = AssignedTask(assigned_task_description="Onboard 2 new employees", owner_id=2)
+    a2 = AssignedTask(assigned_task_description="Contact customers about product", owner_id=2)
+    a3 = AssignedTask(assigned_task_description='Take "Becoming a Manager 101"', owner_id=3)
+    a4 = AssignedTask(assigned_task_description="Fill out finance report", owner_id=3)
     a5 = AssignedTask(assigned_task_description="Complete this assignment", owner_id=1)
 
     db.session.add_all([u1, u2, u3, a1, a2, a3, a4, a5])
